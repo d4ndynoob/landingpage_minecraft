3464ed86c2081880145635de271f8284bc36617c
Daniel Solis <75152170+DanielSolis14@users.noreply.github.com>
2021-06-29 11:33:03 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+const menu = document.getElementById('menu');
+
+menu.addEventListener('click', function(){
+    document.getElementById('cont-links').classList.toggle('nav__cont-links--mostrar');
+});
